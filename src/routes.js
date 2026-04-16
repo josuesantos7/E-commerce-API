db7516c0f8f620705ac4dc0d12495ff6c3b790b5
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2026-04-16 12:35:47 -0300

    configurado o Prisma e criado a primeira rota Create-user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import prisma from "./database/prismaClient.js";
+
 
 const routes = Router();
 
 routes.get("/", (req, res) => {
   return res.json({ message: "API de comércio rodando 🔥" });
+});
+
+routes.get("/create-user", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      name: "Teste1",
+      email: "teste1@email.com",
+      password: "123456"
+    }
+  });
+
+  return res.json(user);
 });
 
 export default routes;
