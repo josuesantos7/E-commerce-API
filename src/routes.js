864d358e15cf4df20a418d4e4b0a40030cff0190
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2026-04-19 00:07:52 -0300

    configurado a função authMiddleware para ler e validar o token, criado a rota profile para teste.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import prisma from "./database/prismaClient.js";
 import { register, login } from "./controllers/authController.js";
-// import { login } from "./controllers/authController.js";
+import { authMiddleware } from "./middlewares/authMiddleware.js";
 
 
 const routes = Router();
@@ -17,5 +17,11 @@ routes.get("/all-usuarios", async (req, res) => {
 
 routes.post("/auth/create-user", register);
 routes.post("/auth/login", login);
+routes.get("/profile", authMiddleware, (req, res) => {
+  return res.json({
+    message: "Acesso autorizado!",
+    userId: req.userId
+  });
+});
 
 export default routes;
