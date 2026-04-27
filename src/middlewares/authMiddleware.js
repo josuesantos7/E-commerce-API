4e6603e442bfadce7497bb073bedeb130293b2a6
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2026-04-27 11:54:21 -0300

    Criado o AuthMiddleware e configurado nas rotas de produto

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -15,6 +15,7 @@ export const authMiddleware = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.userId = decoded.id;
+    req.userRole = decoded.role;
 
     return next();
 
