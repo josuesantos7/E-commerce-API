4a5d445964e8b11f69ba978023eb296e0e074f8e
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2026-05-25 09:30:16 -0300

    Refatoração Controller e criação dos serviços de Product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,14 +1,10 @@
-import prisma from "../database/prismaClient.js";
+import { createProductService, getProductsService, updateProductService, deleteProductService } from "../services/productService.js";
 
 // criar novo produto.
 export const createProduct = async (req, res) => {
   try {
-    const { name, description, price, stock } = req.body;
-
-    const product = await prisma.product.create({
-      data: { name, description, price, stock }
-    });
-
+    const product = await createProductService(req.body);
+    
     return res.status(201).json(product);
   } catch (error) {
     // console.log(error);
@@ -18,30 +14,32 @@ export const createProduct = async (req, res) => {
 
 // listar todos os produtos.
 export const getProducts = async (req, res) => {
-  const products = await prisma.product.findMany();
-  return res.json(products);
+  const products = await getProductsService();
+  return res.status(200).json(products);
 };
 
 // atualizar produto.
 export const updateProduct = async (req, res) => {
-  const { id } = req.params;
-  const { name, description, price, stock } = req.body;
-
-  const product = await prisma.product.update({
-    where: { id },
-    data: { name, description, price, stock }
-  });
-
-  return res.json(product);
+  try {
+    const { id } = req.params;
+    const product = await updateProductService(id, req.body);
+    return res.json(product);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Erro ao atualizar produto" });
+  }
 };
 
 // deletar produto.
 export const deleteProduct = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  await prisma.product.delete({
-    where: { id }
-  });
+    await deleteProductService(id);
 
-  return res.json({ message: "Produto deletado" });
+    return res.json({ message: "Produto deletado com sucesso." });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Erro ao deletar produto" });
+  }
 };
