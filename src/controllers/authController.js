c5d044d12ee64390373eecfcf85dd7eff3900729
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2026-06-05 12:45:43 -0300

    add AppError no authService.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,25 +1,23 @@
 import { registerService, loginService } from "../services/AuthService.js";
 
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const userWithoutPassword = await registerService(req, res);
 
     return res.status(201).json(userWithoutPassword);
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: error.message || "Erro no servidor" });
+    next(error);
   }
 };
 
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
   try {
     const loginResult = await loginService(req, res);
 
     return res.json(loginResult);
 
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: error.message || "Erro no servidor" });
+    next(error);
   }
 };
