5ad5f361cec9dd3fa92005724cdba09aa747996a
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2026-07-24 15:43:08 -0300

    add configuração no CORS

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ import helmet from "helmet";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(routes);
 app.use(errorMiddleware);
