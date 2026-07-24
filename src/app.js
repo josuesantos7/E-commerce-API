7fddcf82ead3126be2ccc473a45ad69e05650a87
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2026-07-24 15:35:36 -0300

    Instalado o Helmet e aplicado no app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 app.use(errorMiddleware);
 
