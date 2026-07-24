0f4ac4c67cc1057bdfa887132319f9e899418d79
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2026-07-24 15:51:58 -0300

    instalação e configuração do rateLimit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import routes from "./routes.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -13,8 +14,19 @@ app.use(cors({
   credentials: true
 }));
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    error: "Muitas requisições. Tente novamente em alguns minutos."
+  }
+});
+app.use(limiter);
+
 app.use(express.json());
 app.use(routes);
 app.use(errorMiddleware);
+
+
 
 export default app;
